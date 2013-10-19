f0a855cf35f7eca7947fd10f3500db79160cbc26
nathanDmoore <moorednathan@gmail.com>
2013-10-19 16:04:56 -0500

    Made changes to API so it uses mysqli

--- API_Server/API.php
+++ API_Server/API.php
@@ -1,162 +1,187 @@
 <?php
 
-function create_deck($username, $deckname)
-{
-  $con = mysql_connect("localhost", "quizard", "quest") or die ("Could not connect: " . mysql_error());
-  mysql_select_db("quizardQuest", $con) or die ("Could not connect: " . mysql_error());
-  
-  mysql_query("INSERT INTO decks (username, name) VALUES ($username, $deckname);");
-  $deckID = mysql_query("SELECT decks.deckID FROM cards WHERE (username = $username AND name = $deckname);");
-  
-  mysql_close($con);
-  return $deckID
-}
+    function create_deck($username, $deckname) {
 
-function create_user($username, $password, $email, $isAdmin)
-{
-  $con = mysql_connect("localhost", "quizard", "quest") or die ("Could not connect: " . mysql_error());
-  mysql_select_db("quizardQuest", $con) or die ("Could not connect: " . mysql_error());
-  
-  $salt = base64_encode(mcrypt_create_iv(PBKDF2_SALT_BYTE_SIZE, MCRYPT_DEV_URANDOM));
-  $password = create_hash_with_salt($password, $salt);
-  
-  mysql_query("INSERT INTO players (username, password, salt, email, permissions) VALUES ($username, $password, $salt, $email, $isAdmin);");
-  mysql_query("INSERT INTO options (username) VALUES ($username);");
-  mysql_query("INSERT INTO achievements (username) VALUES ($username);");
-  mysql_query("INSERT INTO stats (username) VALUES ($username);");
-  
-  mysql_close($con);
-}
+        $db = mysqli_connect("localhost", "quizard", "quest", "quizardQuest");
 
-
-
-
-
-
-// The following is the password salting and hashing functions we found on https://crackstation.net/hashing-security.htm
-/*
- * Password hashing with PBKDF2.
- * Author: havoc AT defuse.ca
- * www: https://defuse.ca/php-pbkdf2.htm
- */
-
-// These constants may be changed without breaking existing hashes.
-define("PBKDF2_HASH_ALGORITHM", "sha256");
-define("PBKDF2_ITERATIONS", 1000);
-define("PBKDF2_SALT_BYTE_SIZE", 24);
-define("PBKDF2_HASH_BYTE_SIZE", 24);
-
-define("HASH_SECTIONS", 4);
-define("HASH_ALGORITHM_INDEX", 0);
-define("HASH_ITERATION_INDEX", 1);
-define("HASH_SALT_INDEX", 2);
-define("HASH_PBKDF2_INDEX", 3);
-
-function create_hash($password)
-{
-    // format: algorithm:iterations:salt:hash
-    $salt = base64_encode(mcrypt_create_iv(PBKDF2_SALT_BYTE_SIZE, MCRYPT_DEV_URANDOM));
-    return PBKDF2_HASH_ALGORITHM . ":" . PBKDF2_ITERATIONS . ":" .  $salt . ":" .
-        base64_encode(pbkdf2(
-            PBKDF2_HASH_ALGORITHM,
-            $password,
-            $salt,
-            PBKDF2_ITERATIONS,
-            PBKDF2_HASH_BYTE_SIZE,
-            true
-        ));
-}
-
-//A function that creates a hash using an input salt. This method is so we can store it in the database beforehand.
-function create_hash_with_salt($password, $salt)
-{
-    // format: algorithm:iterations:salt:hash
-    return PBKDF2_HASH_ALGORITHM . ":" . PBKDF2_ITERATIONS . ":" .  $salt . ":" .
-        base64_encode(pbkdf2(
-            PBKDF2_HASH_ALGORITHM,
-            $password,
-            $salt,
-            PBKDF2_ITERATIONS,
-            PBKDF2_HASH_BYTE_SIZE,
-            true
-        ));
-}
-
-function validate_password($username, $password)
-{
-    $con = mysql_connect("localhost", "quizard", "quest") or die ("Could not connect: " . mysql_error());
-    mysql_select_db("quizardQuest", $con) or die ("Could not connect: " . mysql_error());
-    $correct_hash = mysql_query("SELECT players.password FROM players WHERE (players.username = $username);");
-    $params = explode(":", $correct_hash);
-    if(count($params) < HASH_SECTIONS)
-       return false;
-    $pbkdf2 = base64_decode($params[HASH_PBKDF2_INDEX]);
-    return slow_equals(
-        $pbkdf2,
-        pbkdf2(
-            $params[HASH_ALGORITHM_INDEX],
-            $password,
-            $params[HASH_SALT_INDEX],
-            (int)$params[HASH_ITERATION_INDEX],
-            strlen($pbkdf2),
-            true
-        )
-    );
-}
-
-// Compares two strings $a and $b in length-constant time.
-function slow_equals($a, $b)
-{
-    $diff = strlen($a) ^ strlen($b);
-    for($i = 0; $i < strlen($a) && $i < strlen($b); $i++)
-    {
-        $diff |= ord($a[$i]) ^ ord($b[$i]);
-    }
-    return $diff === 0;
-}
-
-/*
- * PBKDF2 key derivation function as defined by RSA's PKCS #5: https://www.ietf.org/rfc/rfc2898.txt
- * $algorithm - The hash algorithm to use. Recommended: SHA256
- * $password - The password.
- * $salt - A salt that is unique to the password.
- * $count - Iteration count. Higher is better, but slower. Recommended: At least 1000.
- * $key_length - The length of the derived key in bytes.
- * $raw_output - If true, the key is returned in raw binary format. Hex encoded otherwise.
- * Returns: A $key_length-byte key derived from the password and salt.
- *
- * Test vectors can be found here: https://www.ietf.org/rfc/rfc6070.txt
- *
- * This implementation of PBKDF2 was originally created by https://defuse.ca
- * With improvements by http://www.variations-of-shadow.com
- */
-function pbkdf2($algorithm, $password, $salt, $count, $key_length, $raw_output = false)
-{
-    $algorithm = strtolower($algorithm);
-    if(!in_array($algorithm, hash_algos(), true))
-        die('PBKDF2 ERROR: Invalid hash algorithm.');
-    if($count <= 0 || $key_length <= 0)
-        die('PBKDF2 ERROR: Invalid parameters.');
-
-    $hash_length = strlen(hash($algorithm, "", true));
-    $block_count = ceil($key_length / $hash_length);
-
-    $output = "";
-    for($i = 1; $i <= $block_count; $i++) {
-        // $i encoded as 4 bytes, big endian.
-        $last = $salt . pack("N", $i);
-        // first iteration
-        $last = $xorsum = hash_hmac($algorithm, $last, $password, true);
-        // perform the other $count - 1 iterations
-        for ($j = 1; $j < $count; $j++) {
-            $xorsum ^= ($last = hash_hmac($algorithm, $last, $password, true));
+        if (mysqli_connect_errno()) {
+            printf("Connect failed: %s\n", mysqli_connect_errno());
+            exit();
         }
-        $output .= $xorsum;
+      
+        mysqli_query($db,"INSERT INTO decks (username, name) VALUES ($username, $deckname);");
+        $deckID = mysqli_query($db,"SELECT decks.deckID FROM cards WHERE (username = $username AND name = $deckname);");
+
+        if (!mysqli_query($db,$insertAllQuery)) {
+            echo "There was an error processing your request. Please return to the previous page. Here's the error if you wanted to know:\n";
+            die('Error: ' . mysqli_error($db));
+        }
+
+        $idRow = mysqli_fetch_assoc($deckID);
+
+        $id = intval($idRow['decks.deckID']);
+      
+        mysqli_close($db);
+        return $deckID
     }
 
-    if($raw_output)
-        return substr($output, 0, $key_length);
-    else
-        return bin2hex(substr($output, 0, $key_length));
-}
+    function create_user($username, $password, $email, $isAdmin) {
+
+        $db = mysqli_connect("localhost", "quizard", "quest", "quizardQuest");
+      
+        if (mysqli_connect_errno()) {
+            printf("Connect failed: %s\n", mysqli_connect_errno());
+            exit();
+        }
+      
+        $salt = base64_encode(mcrypt_create_iv(PBKDF2_SALT_BYTE_SIZE, MCRYPT_DEV_URANDOM));
+        $password = create_hash_with_salt($password, $salt);
+      
+        mysqli_query($db,"INSERT INTO players (username, password, salt, email, permissions) VALUES ($username, $password, $salt, $email, $isAdmin);");
+        mysqli_query($db,"INSERT INTO options (username) VALUES ($username);");
+        mysqli_query($db,"INSERT INTO achievements (username) VALUES ($username);");
+        mysqli_query($db,"INSERT INTO stats (username) VALUES ($username);");
+
+        mysqli_close($db);
+    }
+
+
+
+
+
+
+    // The following is the password salting and hashing functions we found on https://crackstation.net/hashing-security.htm
+    /*
+     * Password hashing with PBKDF2.
+     * Author: havoc AT defuse.ca
+     * www: https://defuse.ca/php-pbkdf2.htm
+     */
+
+    // These constants may be changed without breaking existing hashes.
+    define("PBKDF2_HASH_ALGORITHM", "sha256");
+    define("PBKDF2_ITERATIONS", 1000);
+    define("PBKDF2_SALT_BYTE_SIZE", 24);
+    define("PBKDF2_HASH_BYTE_SIZE", 24);
+
+    define("HASH_SECTIONS", 4);
+    define("HASH_ALGORITHM_INDEX", 0);
+    define("HASH_ITERATION_INDEX", 1);
+    define("HASH_SALT_INDEX", 2);
+    define("HASH_PBKDF2_INDEX", 3);
+
+    function create_hash($password) {
+        // format: algorithm:iterations:salt:hash
+        $salt = base64_encode(mcrypt_create_iv(PBKDF2_SALT_BYTE_SIZE, MCRYPT_DEV_URANDOM));
+        return PBKDF2_HASH_ALGORITHM . ":" . PBKDF2_ITERATIONS . ":" .  $salt . ":" .
+            base64_encode(pbkdf2(
+                PBKDF2_HASH_ALGORITHM,
+                $password,
+                $salt,
+                PBKDF2_ITERATIONS,
+                PBKDF2_HASH_BYTE_SIZE,
+                true
+            ));
+    }
+
+    //A function that creates a hash using an input salt. This method is so we can store it in the database beforehand.
+    function create_hash_with_salt($password, $salt) {
+        // format: algorithm:iterations:salt:hash
+        return PBKDF2_HASH_ALGORITHM . ":" . PBKDF2_ITERATIONS . ":" .  $salt . ":" .
+            base64_encode(pbkdf2(
+                PBKDF2_HASH_ALGORITHM,
+                $password,
+                $salt,
+                PBKDF2_ITERATIONS,
+                PBKDF2_HASH_BYTE_SIZE,
+                true
+            ));
+    }
+
+    function validate_password($username, $password) {
+
+        $db = mysqli_connect("localhost", "quizard", "quest", "quizardQuest");
+      
+        if (mysqli_connect_errno()) {
+            printf("Connect failed: %s\n", mysqli_connect_errno());
+            exit();
+        }
+
+        $correct_hash = mysqli_query($db,"SELECT players.password FROM players WHERE (players.username = $username);");
+        $params = explode(":", $correct_hash);
+        if(count($params) < HASH_SECTIONS) {
+            mysqli_close($db);
+            return false;
+        }
+           
+        $pbkdf2 = base64_decode($params[HASH_PBKDF2_INDEX]);
+        return slow_equals(
+            $pbkdf2,
+            pbkdf2(
+                $params[HASH_ALGORITHM_INDEX],
+                $password,
+                $params[HASH_SALT_INDEX],
+                (int)$params[HASH_ITERATION_INDEX],
+                strlen($pbkdf2),
+                true
+            )
+        );
+
+        mysqli_close($db);
+    }
+
+    // Compares two strings $a and $b in length-constant time.
+    function slow_equals($a, $b)  {
+
+        $diff = strlen($a) ^ strlen($b);
+        for($i = 0; $i < strlen($a) && $i < strlen($b); $i++)
+        {
+            $diff |= ord($a[$i]) ^ ord($b[$i]);
+        }
+        return $diff === 0;
+    }
+
+    /*
+     * PBKDF2 key derivation function as defined by RSA's PKCS #5: https://www.ietf.org/rfc/rfc2898.txt
+     * $algorithm - The hash algorithm to use. Recommended: SHA256
+     * $password - The password.
+     * $salt - A salt that is unique to the password.
+     * $count - Iteration count. Higher is better, but slower. Recommended: At least 1000.
+     * $key_length - The length of the derived key in bytes.
+     * $raw_output - If true, the key is returned in raw binary format. Hex encoded otherwise.
+     * Returns: A $key_length-byte key derived from the password and salt.
+     *
+     * Test vectors can be found here: https://www.ietf.org/rfc/rfc6070.txt
+     *
+     * This implementation of PBKDF2 was originally created by https://defuse.ca
+     * With improvements by http://www.variations-of-shadow.com
+     */
+    function pbkdf2($algorithm, $password, $salt, $count, $key_length, $raw_output = false) {
+
+        $algorithm = strtolower($algorithm);
+        if(!in_array($algorithm, hash_algos(), true))
+            die('PBKDF2 ERROR: Invalid hash algorithm.');
+        if($count <= 0 || $key_length <= 0)
+            die('PBKDF2 ERROR: Invalid parameters.');
+
+        $hash_length = strlen(hash($algorithm, "", true));
+        $block_count = ceil($key_length / $hash_length);
+
+        $output = "";
+        for($i = 1; $i <= $block_count; $i++) {
+            // $i encoded as 4 bytes, big endian.
+            $last = $salt . pack("N", $i);
+            // first iteration
+            $last = $xorsum = hash_hmac($algorithm, $last, $password, true);
+            // perform the other $count - 1 iterations
+            for ($j = 1; $j < $count; $j++) {
+                $xorsum ^= ($last = hash_hmac($algorithm, $last, $password, true));
+            }
+            $output .= $xorsum;
+        }
+
+        if($raw_output)
+            return substr($output, 0, $key_length);
+        else
+            return bin2hex(substr($output, 0, $key_length));
+    }
 ?>
