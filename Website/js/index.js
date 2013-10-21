89a46b9fb940da3206c54b19251afa456c8a18d1
nathanDmoore <moorednathan@gmail.com>
2013-10-21 09:38:31 -0500

    Added index.js to js folder in Website folder

---
+++ Website/js/index.js
@@ -0,0 +1,42 @@
+//Author: Nathan Moore
+//Scripts to run in concert with index.html
+
+$(document).ready(function() {
+
+	/*\
+	|*|				:: >> Authenticate User After Login << ::
+	|*|
+	|*|		# add a click listener to the submit button associated 
+	|*|			with the log in form (not the create account form)
+	|*|		# make an ajax request that sends the form data to 
+	|*|			logIn.php for authentication
+	|*|		# if it is authenticated (i.e., success) the page will 
+	|*|			be redirected to the cupcakeordering.html page
+	|*|		# otherwise an alert will go off and form submission will 
+	|*|			be aborted
+	|*|
+	\*/
+	
+	$("#logInButton").click(function(e) {
+		var username = $("#user").val();
+		var password = $("#password").val();
+
+		var formData = {pass: password, user: username};
+
+		$.post("../API_Server/logIn.php",formData,function(data){
+
+			if(data['success']) {
+            	// do successful things
+            	window.location.href = "";
+        	}
+        	else {
+            	// do failure things
+            	alert("Username or Password is Invalid!");
+        	}
+		},"json");
+
+		e.preventDefault();
+
+	});
+
+});
