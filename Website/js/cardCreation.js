d036e287c0996c1c4243ea011b87a95d4269b39d
nathanDmoore <moorednathan@gmail.com>
2013-10-22 18:34:45 -0500

    Add preven default

--- Website/js/cardCreation.js
+++ Website/js/cardCreation.js
@@ -1,23 +1,25 @@
 $(document).ready(function(){
 
 
-$("#submitQuestion").click(function(e){
+	$("#submitQuestion").click(function(e){
 
-	var category = document.getElementById('categorySelect').value;
-	var subcategory = document.getElementById('newSubcategory').value;
-	var question = document.getElementById('newQuestion').value;
-	var answer = document.getElementById('newAnswer').value;
-	var difficulty = document.getElementById('newDifficulty').value;
+		var category = document.getElementById('categorySelect').value;
+		var subcategory = document.getElementById('newSubcategory').value;
+		var question = document.getElementById('newQuestion').value;
+		var answer = document.getElementById('newAnswer').value;
+		var difficulty = document.getElementById('newDifficulty').value;
 
-	alert(category + subcategory + question + answer + difficulty);
+		alert(category + subcategory + question + answer + difficulty);
 
 
-	var formData = {'category':category,'subcategory':subcategory,'question':question, 'answer':answer, 'difficulty':difficulty};
+		var formData = {'category':category,'subcategory':subcategory,'question':question, 'answer':answer, 'difficulty':difficulty};
 
-	$.post("../API_Server/cardCreation.php",formData,function(){
-		alert("Question Added");
-	},"json");
+		$.post("../API_Server/cardCreation.php",formData,function(){
+			alert("Question Added");
+		},"json");
 
-});
+		e.preventDefault();
+
+	});
 
 });
