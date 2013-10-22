8ac392984bc055adfb24a3995d506c7a68e47c07
MissingNo <spacepirate92@gmail.com>
2013-10-22 18:08:40 -0500

    Fixed javascript error

--- Website/js/cardCreation.js
+++ Website/js/cardCreation.js
@@ -3,11 +3,11 @@ $(document).ready(function(){
 
 $("#submitQuestion").click(function(e){
 
-	var category = document.getElementById('categorySelect').getAttribute("value");
-	var subcategory = document.getElementById('newSubcategory').getAttribute("value");
-	var question = document.getElementById('newQuestion').getAttribute("value");
-	var answer = document.getElementById('newAnswer').getAttribute("value");
-	var difficulty = document.getElementById('newDifficulty').getAttribute("value");
+	var category = document.getElementById('categorySelect').value;
+	var subcategory = document.getElementById('newSubcategory').value;
+	var question = document.getElementById('newQuestion').value;
+	var answer = document.getElementById('newAnswer').value;
+	var difficulty = document.getElementById('newDifficulty').value;
 	var formData = {category:category,subcategory:subcategory,question:question, answer:answer, difficulty:difficulty};
 
 	$.post("../API_Server/cardCreation.php",formData,function(){
