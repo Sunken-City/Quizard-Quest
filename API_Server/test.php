be1a9ec30a6fd6be6b23d03700a6e943bee9d858
picoriley <acloudy@mail.smu.edu>
2013-10-23 15:51:17 -0500

    Attempting to create a user first

--- API_Server/test.php
+++ API_Server/test.php
@@ -1,5 +1,6 @@
 <?php
   include 'API.php';
+  create_user('Pico', 'Riley', 'a@a.a', 'picoriley', '12341234', NULL, NULL, 0);
   $deckID = create_deck('Pico', 'PicoDeck');
   echo "<h1> $deckID </h1>";
 ?>
