b761f7ebd4c2dd7c63453ab67535187778887b9d
picoriley <acloudy@mail.smu.edu>
2013-10-23 16:15:24 -0500

    That worked. Trying another deck as well

--- API_Server/test.php
+++ API_Server/test.php
@@ -1,6 +1,8 @@
 <?php
   include 'API.php';
+  
+  
   //create_user('Pico', 'Riley', 'a@a.a', 'picoriley', '12341234', NULL, NULL, 0);
-  $deckID = create_deck('picoriley', 'PicoDeck');
+  $deckID = create_deck('picoriley', 'PicoDeck2');
   echo "<h1> $deckID </h1>";
 ?>
