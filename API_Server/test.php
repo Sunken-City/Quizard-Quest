8113e1ba3c84795ab0943eaebd828092c46c4a0f
picoriley <acloudy@mail.smu.edu>
2013-10-23 16:01:10 -0500

    Creating a user works. Now trying to add a deck using the create_deck query

--- API_Server/test.php
+++ API_Server/test.php
@@ -1,6 +1,6 @@
 <?php
   include 'API.php';
-  create_user('Pico', 'Riley', 'a@a.a', 'picoriley', '12341234', NULL, NULL, 0);
-  $deckID = create_deck('Pico', 'PicoDeck');
+  //create_user('Pico', 'Riley', 'a@a.a', 'picoriley', '12341234', NULL, NULL, 0);
+  $deckID = create_deck('picoriley', 'PicoDeck');
   echo "<h1> $deckID </h1>";
 ?>
