32adff109a13765117f74b5e96fc6f0882bce2fe
nathanDmoore <moorednathan@gmail.com>
2013-10-24 16:01:26 -0500

    Working out the log out and destroying a session: Added a comma that was missing

--- Website/js/navigation.js
+++ Website/js/navigation.js
@@ -3,9 +3,9 @@ $(document).ready(function() {
 	$("#logOut").click(function(e) {
 
 		$.ajax({
-			url: "../API_Server/logOut.php"
+			url: "../API_Server/logOut.php",
 			success: function () {
-				window.location.href = "index.html";
+				window.location.href = "http://54.200.66.93/Quizard-Quest/Website/";
 				//http://54.200.66.93/Quizard-Quest/Website/
 			}
 		});
