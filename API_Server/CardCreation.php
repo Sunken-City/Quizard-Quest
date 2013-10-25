c8495234f1885087c2af0d6d350331dca683d95a
MissingNo <pdenicola@smu.edu>
2013-10-25 14:03:31 -0500

    Changed username to userId

--- API_Server/CardCreation.php
+++ API_Server/CardCreation.php
@@ -8,7 +8,7 @@
 	include "API.php";
 	session_start();
 
-	$username = $_SESSION['userID'];
+	$userID = $_SESSION['userID'];
 	$category = $_POST['category'];
 	$subcategory = $_POST['subcategory'];
 	$question = $_POST['question'];
@@ -18,6 +18,6 @@
 
 	//$file = "../Design_Documents/test.txt";
 	//file_put_contents($file, $category);
-	$card = create_card($username,$question,$answer,$category,$subcategory,$difficulty);
+	$card = create_card($userID,$question,$answer,$category,$subcategory,$difficulty);
 
 ?>
