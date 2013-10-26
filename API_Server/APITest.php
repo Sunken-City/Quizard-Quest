8efc16acb8f45d3fa36364c91e3b4f038fa929c0
picoriley <acloudy@mail.smu.edu>
2013-10-26 15:53:41 -0500

    Created first unit test for testing the API

---
+++ API_Server/APITest.php
@@ -0,0 +1,18 @@
+<?php
+    /*\
+    |*|     :: >>UNIT TESTING ON THE API SERVER FOR QUIZZARD QUEST<< ::
+    \*/
+include 'API.php';
+
+class APITest extends PHPUnit_Framework_TestCase
+{
+  //Unit test that attempts to create a user.
+  public function testCreateUser()
+  {
+    $response = create_user('Pico', 'Riley', 'a@a.a', 'picoriley', '12341234', NULL, NULL, 0);
+    $this->assertTrue($response);
+  }
+  
+}
+
+?>
