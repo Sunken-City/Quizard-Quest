9be752997dbcf3f7f0d0b1a655db0152bc3913e0
Gustavo Castillo <gcastillo@smu.edu>
2013-10-28 16:29:06 -0500

    Testing the cards returned in deck create

--- Website/js/deckCreate.js
+++ Website/js/deckCreate.js
@@ -2,6 +2,12 @@ $(document).ready(function(){
 
 	$("#createDeck").click(function(e){\
 
+	var JSON;
+	$.post("../API_Server/DeckCreation.php",formData,function(data){
+		JSON = data;
+		},"json");
+	alert(JSON['question']);
+	
 	/* Get all the TD that are checked
 	Submit them to a PHP function in an array
 	PHP function makes the Deck
