60b80f1199f058106bb04c2df32576dd657f7301
Gustavo Castillo <gcastillo@smu.edu>
2013-10-28 00:36:50 -0500

    Adding the JS file for deck creation and did some dummy data in deck creation html file Will modify the css a bit later

---
+++ Website/js/deckCreate.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+	$("#createDeck").click(function(e){\
+
+	/* Get all the TD that are checked
+	Submit them to a PHP function in an array
+	PHP function makes the Deck
+	*/
+
+	/*Also needs a way to import all question from the Database */
+
+		
+	});
+
+});
