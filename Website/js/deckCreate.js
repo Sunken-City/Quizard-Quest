1db7aec27dcb8e8528eb88e664b91c6170ca744a
moosemn456 <jgstangelo@gmail.com>
2013-10-28 16:35:30 -0500

    Changed createDeck.js

--- Website/js/deckCreate.js
+++ Website/js/deckCreate.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
 	$.post("../API_Server/DeckCreation.php",formData,function(data){
 		JSON = data;
 		},"json");
-	alert(JSON['question']);
+	alert("Hello");
 	
 	/* Get all the TD that are checked
 	Submit them to a PHP function in an array
