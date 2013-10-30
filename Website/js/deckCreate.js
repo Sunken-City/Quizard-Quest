4d7f45aad78022ee9b3752318ac5a6429aee230a
Gustavo Castillo <gcastillo@smu.edu>
2013-10-29 23:13:53 -0500

    Attempting to fix the deckcreate javascript

--- Website/js/deckCreate.js
+++ Website/js/deckCreate.js
@@ -2,22 +2,23 @@ $(document).ready(function(){
 
 	$("#createDeck").click(function(e){
 
-	e.preventDefault();
+	
 	var JSON;
 
 	$.post("../API_Server/getCards.php", function(data){
 		
 		JSON = $.parseJSON(data);
 		});
-	alert(JSON.question);
-	
+
+	alert(JSON[0]);
+	e.preventDefault();
 	/* Get all the TD that are checked
 	Submit them to a PHP function in an array
 	PHP function makes the Deck
 	*/
 
 	/*Also needs a way to import all question from the Database */
-
+	
 		
 	});
 
