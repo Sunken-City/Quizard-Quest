5def6bac4e0108f1e76780d6c939b4d3fdffdc19
Gustavo Castillo <gcastillo@smu.edu>
2013-10-29 23:51:01 -0500

    Attempting to fix the deckcreate javascript

--- Website/js/deckCreate.js
+++ Website/js/deckCreate.js
@@ -10,6 +10,20 @@ $.post("../API_Server/getCards.php", function(data){
 	JSON = $.parseJSON(data);
 });
 
+function FlashCard(){
+	var question;
+	var answer;
+	var cardID;
+	var subCategory;
+}
+
+for( var i = 0, len = JSON.length;  i < len; i++){
+	var tableData = document.createElement("td");
+	var question = document.createElement("p");
+	question.innerHTML = JSON[i].question
+	tableData.appendChild(question);
+	body.getElementById('UserCards').appendChild(tableData);
+}
 
 $(document).ready(function(){
 
