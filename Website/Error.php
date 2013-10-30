d6cf6ec77e2f723405abfd001529fd61903b9055
picoriley <acloudy@mail.smu.edu>
2013-10-30 18:27:56 -0500

    Weird { error. Looking into it

--- Website/Error.php
+++ Website/Error.php
@@ -8,20 +8,23 @@ echo "<html lang=\"en\">
 	  <script type = \"text/javascript\" src=\"js/Error.js\"></script>
 	  <link href='http://fonts.googleapis.com/css?family=Share+Tech+Mono|VT323' rel='stylesheet' type='text/css'>
   </head>";
-  if(isset($_GET['err']) 
-  {
+  if(isset($_GET['err']) {
     if($_GET['err'] == 100)
     {
       echo "<body>The username you entered is already associated with a user.
                  Please return to the previous page and log in with your 
                  password or use a different username</body>";
     }
-    if($_GET['err'] == 101)
+    else if($_GET['err'] == 101)
     {
       echo "<body>The email you entered is already associated with a user.
                  Please return to the previous page and log in with your 
                  password or use a different email</body>";
     }
+  }
+  else
+  {
+    echo "<body>No problems here!</body>";
   }
     
 
