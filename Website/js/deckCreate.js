580dcc5f745409ae66455730b29c9aec9f25d960
Gustavo Castillo <gcastillo@smu.edu>
2013-10-29 23:24:39 -0500

    Attempting to fix the deckcreate javascript

--- Website/js/deckCreate.js
+++ Website/js/deckCreate.js
@@ -10,12 +10,11 @@ $.post("../API_Server/getCards.php", function(data){
 	JSON = $.parseJSON(data);
 });
 
-alert(JSON[0]);
 
 $(document).ready(function(){
 
 	$("#createDeck").click(function(e){
-	
+	alert(JSON[0]);
 	e.preventDefault();
 	/* Get all the TD that are checked
 	Submit them to a PHP function in an array
