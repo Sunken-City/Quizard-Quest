17c119d90e06c6b6a43e343bc50135381393e5c9
Gustavo Castillo <gcastillo@smu.edu>
2013-10-29 20:37:15 -0500

    Attempting to fix the deckcreate javascript

--- Website/js/deckCreate.js
+++ Website/js/deckCreate.js
@@ -3,13 +3,12 @@ $(document).ready(function(){
 	$("#createDeck").click(function(e){
 
 	var JSON;
-	var pJSON;
-	$.post("../API_Server/DeckCreation.php",function(data){
-		JSON = data;
-		pJSON = JSON.parse(JSON);
+
+	$.getJSON("../API_Server/DeckCreation.php",function(data){
+		JSON = JSON.parse(data);
 		alert(pJSON.question);
 		window.location.href = "DeckCreate.html";
-		},"json");
+		});
 	e.preventDefault();
 	/* Get all the TD that are checked
 	Submit them to a PHP function in an array
