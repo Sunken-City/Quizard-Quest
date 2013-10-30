f618b229da150f29726221cbc20adb113ce3ddd4
picoriley <acloudy@mail.smu.edu>
2013-10-30 18:23:07 -0500

    Forgot another semicolon >:I

--- Website/Error.php
+++ Website/Error.php
@@ -7,7 +7,7 @@ echo "<html lang="en">
 	  <script type = \"text/javascript\" src=\"https://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js\"></script>
 	  <script type = \"text/javascript\" src=\"js/Error.js\"></script>
 	  <link href='http://fonts.googleapis.com/css?family=Share+Tech+Mono|VT323' rel='stylesheet' type='text/css'>
-  </head>"
+  </head>";
   if(isset($_GET['err']) 
   {
     if($_GET['err'] == 100)
