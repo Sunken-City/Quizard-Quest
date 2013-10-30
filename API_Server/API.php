17710f83536a0f73956664f7a51e8aa39a6f33c6
picoriley <acloudy@mail.smu.edu>
2013-10-30 17:42:19 -0500

    Another attempt at making errors pretty

--- API_Server/API.php
+++ API_Server/API.php
@@ -123,9 +123,14 @@
         /*make sure email isn't already in the database*/
         $query = "SELECT * FROM players WHERE (email = '$email');";
         if (!mysqli_query($db, $query)) {
-            echo "
-                    <head>
+            echo "There was an error checking to see if your email already exists. Please return to the previous page.
+		Here's the error if you wanted to tell a developer:\n";
+            die('Error: ' . mysqli_error($db));
+        }
+        $result = mysqli_query($db,$query);
 
+        if (!(mysqli_num_rows($result) == 0)) {
+	    echo "                <head>
                 <title>QQ: Main Menu</title>
                 <link rel=\"stylesheet\" href=\"css/mainStyle.css\"/>
                 <meta charset=\"UTF-8\">
@@ -135,17 +140,13 @@
                 <link href='http://fonts.googleapis.com/css?family=Share+Tech+Mono|VT323' rel='stylesheet' type='text/css'>
                 </head>
                 <body>
-		There was an error checking to see if your email already exists. Please return to the previous page.
-		Here's the error if you wanted to tell a developer:\n
-		</body>";
-            die('Error: ' . mysqli_error($db));
-        }
-        $result = mysqli_query($db,$query);
-
-        if (!(mysqli_num_rows($result) == 0)) {
-            die('The email you entered is already associated with a user.
+                The email you entered is already associated with a user.
                  Please return to the previous page and log in with your 
-                 password or use a different email.');
+                 password or use a different email.
+                </body>"
+            /*die('The email you entered is already associated with a user.
+                 Please return to the previous page and log in with your 
+                 password or use a different email.');*/
             return false;
         }
 
