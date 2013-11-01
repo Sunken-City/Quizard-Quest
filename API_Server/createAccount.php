ee1a81f53f102cf84fb2b557a5040a364d8bba3e
picoriley <acloudy@mail.smu.edu>
2013-11-01 18:01:27 -0500

    I swear if the bug is this space I'm going to cry

--- API_Server/createAccount.php
+++ API_Server/createAccount.php
@@ -76,7 +76,7 @@
                 /* Nathan's EC2: Location: http://54.200.66.93/Quizard-Quest/Website/mainMenu.html */
                 $_SESSION['redirected'] = "createAccount";
 
-                header('Location: http://'.$url.'/Quizard-Quest/Website/mainMenu.php');
+                header('Location:http://'.$url.'/Quizard-Quest/Website/mainMenu.php');
         // }
 
         // echo json_encode($timedOut);
