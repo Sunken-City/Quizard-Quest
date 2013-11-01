5d61d140a8a77e5de8fc2462c16cfb1699d3431b
picoriley <acloudy@mail.smu.edu>
2013-11-01 16:39:14 -0500

    Fixed issue with index.php

--- API_Server/index.php
+++ API_Server/index.php
@@ -1,4 +1,4 @@
 <?php
 include 'url.php';
-header('Location:http://'.$url.'Quizard-Quest/Website/Error.php?err=404');
+header('Location:http://'.$url.'/Quizard-Quest/Website/Error.php?err=404');
 ?>
