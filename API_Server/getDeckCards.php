e3fc1a5317dcccb7ec6c14e21a37a1faa5c19db5
Gustavo Castillo <gcastillo@smu.edu>
2013-11-03 22:13:58 -0600

    Testing the edit deck function

--- API_Server/getDeckCards.php
+++ API_Server/getDeckCards.php
@@ -3,9 +3,7 @@
 
 	include "API.php";
 	session_start();
-   $deckID = $_POST['deckID'];
-	
 	//file_put_contents($file, $category);
-        echo json_encode(get_deck_cards($deckID));
+        echo json_encode(get_deck_cards($q));
 	
 ?>
