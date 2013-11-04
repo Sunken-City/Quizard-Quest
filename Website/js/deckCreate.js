aeabd689a3fbe1acd4e6e0e2848aad88d4f1945e
nathanDmoore <moorednathan@gmail.com>
2013-11-04 17:22:36 -0600

    Scaling down the size of cards previewed in deck creation

--- Website/js/deckCreate.js
+++ Website/js/deckCreate.js
@@ -34,13 +34,13 @@ $.post("../API_Server/getCards.php", function(data){
 		checkBox.type = "checkbox";
 		checkBox.name = "flashCard[]";
 
-		card.className = "card";
-		cardQuestion.className = "cardQuestion";
-		cardAnswer.className = "cardAnswer";
-		cardDifficulty.className = "cardDifficulty";
-		cardCategory.className = "cat cardCategory";
-		cardSubcategory.className = "cat subCat cardSubcategory";
-		cardTitles.className = "cardTitles";
+		card.className = "card deckCreate";
+		cardQuestion.className = "cardQuestion deckCreate";
+		cardAnswer.className = "cardAnswer deckCreate";
+		cardDifficulty.className = "cardDifficulty deckCreate";
+		cardCategory.className = "cat cardCategory deckCreate";
+		cardSubcategory.className = "cat subCat cardSubcategory deckCreate";
+		cardTitles.className = "cardTitles deckCreate";
 		var difficultyValue = JSON[i].difficulty;
 		var categoryValue = JSON[i].category;
 		var categoryName;
