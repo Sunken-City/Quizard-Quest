0a2f027deef7783ede393d13cc80ef6ca02352ae
Gustavo Castillo <gcastillo@smu.edu>
2013-11-03 22:56:37 -0600

    Made a small change to see how json decode works

--- API_Server/getDeckCards.php
+++ API_Server/getDeckCards.php
@@ -6,7 +6,6 @@
 	//file_put_contents($file, $category);
         $deckCards =  get_non_deck_cards($q);
 	$test = json_decode($deckCards,true);
-	var_dump($test);
 	
 	foreach ($test as $card){
 		echo $card;
