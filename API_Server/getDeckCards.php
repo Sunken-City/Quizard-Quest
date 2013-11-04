08608de82fc63b8026575a3024151f5b3c923620
Gustavo Castillo <gcastillo@smu.edu>
2013-11-03 22:27:24 -0600

    Testing the printing of a JSON in PHP

--- API_Server/getDeckCards.php
+++ API_Server/getDeckCards.php
@@ -4,7 +4,7 @@
 	include "API.php";
 	session_start();
 	//file_put_contents($file, $category);
-        $deckCards =  get_deck_cards($q);
+        $deckCards =  json_encode(get_deck_cards($q));
 	
 	foreach($deckCards as $card){
 		
