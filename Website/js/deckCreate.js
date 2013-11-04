766dfdcb9e08e9b1c4de56766952409621f0e40f
nathanDmoore <moorednathan@gmail.com>
2013-11-04 16:53:03 -0600

    Trying to make the cards appear like cards when creating a deck

--- Website/js/deckCreate.js
+++ Website/js/deckCreate.js
@@ -4,54 +4,54 @@ var JSON;
 
 $.post("../API_Server/getCards.php", function(data){
 	JSON = $.parseJSON(data);
-	// for( var i = 0, len = JSON.length;  i < len; i++){
-	// 	console.log(i);
-	// 	var tableData = document.createElement("td");
-	// 	var question = document.createElement("p");
-	// 	var checkBox = document.createElement("input");
-	// 	checkBox.value = JSON[i].cardID;
-	// 	checkBox.type = "checkbox";
-	// 	checkBox.name = "flashCard[]";
-	// 	question.innerHTML = "Question: " + JSON[i].question + " \nAnswer: " + JSON[i].answer;
-	// 	tableData.appendChild(checkBox);
-	// 	tableData.appendChild(question);
-	// 	document.getElementById('UserCards').appendChild(tableData);
-	// }
-
-	for (var i = 0, len = JSON.length; i < len; i++) {
-
-		var td = document.createElement("td");
-		var card = document.createElement("div");
-		var cardTitles = document.createElement("div");
-		var cardDifficulty = document.createElement("div");
-		var cardCategory = document.createElement("div");
-		var cardSubcategory = document.createElement("div");
-		var cardQuestion = document.createElement("div");
-		var cardAnswer = document.createElement("div");
+	for( var i = 0, len = JSON.length;  i < len; i++){
+		console.log(i);
+		var tableData = document.createElement("td");
+		var question = document.createElement("p");
 		var checkBox = document.createElement("input");
-
 		checkBox.value = JSON[i].cardID;
 		checkBox.type = "checkbox";
-		checkBox.name = "flashCard[]"
-
-		cardQuestion.innerHTML = "Q: " + JSON[i].question;
-		cardAnswer.innerHTML = "A: " + JSON[i].answer;
-		cardDifficulty.innerHTML = JSON[i].difficulty;
-		cardCategory.innerHTML = JSON[i].category + ": ";
-		cardSubcategory = JSON[i].subCategory;
-
-		cardTitles.appendChild(cardDifficulty);
-		cardTitles.appendChild(cardCategory);
-		cardTitles.appendChild(cardSubcategory);
-		card.appendChild(cardTitles);
-		card.appendChild(cardQuestion);
-		card.appendChild(cardAnswer);
-		td.appendChild(checkbox);
-		td.appendChild(card);
-		document.getElementById('UserCards').appendChild(td);
-
-		$(".card").show();
+		checkBox.name = "flashCard[]";
+		question.innerHTML = "Question: " + JSON[i].question + " \nAnswer: " + JSON[i].answer;
+		tableData.appendChild(checkBox);
+		tableData.appendChild(question);
+		document.getElementById('UserCards').appendChild(tableData);
 	}
+
+	// for (var i = 0, len = JSON.length; i < len; i++) {
+
+	// 	var td = document.createElement("td");
+	// 	var card = document.createElement("div");
+	// 	var cardTitles = document.createElement("div");
+	// 	var cardDifficulty = document.createElement("div");
+	// 	var cardCategory = document.createElement("div");
+	// 	var cardSubcategory = document.createElement("div");
+	// 	var cardQuestion = document.createElement("div");
+	// 	var cardAnswer = document.createElement("div");
+	// 	var checkBox = document.createElement("input");
+
+	// 	checkBox.value = JSON[i].cardID;
+	// 	checkBox.type = "checkbox";
+	// 	checkBox.name = "flashCard[]"
+
+	// 	cardQuestion.innerHTML = "Q: " + JSON[i].question;
+	// 	cardAnswer.innerHTML = "A: " + JSON[i].answer;
+	// 	cardDifficulty.innerHTML = JSON[i].difficulty;
+	// 	cardCategory.innerHTML = JSON[i].category + ": ";
+	// 	cardSubcategory = JSON[i].subCategory;
+
+	// 	cardTitles.appendChild(cardDifficulty);
+	// 	cardTitles.appendChild(cardCategory);
+	// 	cardTitles.appendChild(cardSubcategory);
+	// 	card.appendChild(cardTitles);
+	// 	card.appendChild(cardQuestion);
+	// 	card.appendChild(cardAnswer);
+	// 	td.appendChild(checkbox);
+	// 	td.appendChild(card);
+	// 	document.getElementById('UserCards').appendChild(td);
+
+	// 	$(".card").show();
+	// }
 
 	/** Format to output for cards **
 	<div class="card">
