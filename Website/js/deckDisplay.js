cb8c38324196ec870af4409aec586bde72341f88
Gustavo Castillo <gcastillo@smu.edu>
2013-11-04 16:21:02 -0600

    Change to the Deck Edit page

--- Website/js/deckDisplay.js
+++ Website/js/deckDisplay.js
@@ -26,7 +26,7 @@ function showDeck(str)
   	{
   	if (xmlhttp.readyState==4 && xmlhttp.status==200)
    	 {
-   	 document.getElementById("cardRemove").innerHTML=xmlhttp.responseText;
+   	 document.getElementById("deckCards").innerHTML=xmlhttp.responseText;
   	  }
  	 }
 	xmlhttp.open("GET","../API_Server/getDeckCards.php?q="+str,true);
