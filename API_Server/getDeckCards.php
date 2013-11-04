dace96c2e053b6b50ae1f5531ba0607b12372033
Gustavo Castillo <gcastillo@smu.edu>
2013-11-04 15:47:43 -0600

    Change to the Deck Edit page

--- API_Server/getDeckCards.php
+++ API_Server/getDeckCards.php
@@ -4,7 +4,7 @@
 	include "API.php";
 	session_start();
 	//file_put_contents($file, $category);
-        $test =  json_encode(get_deck_cards($q));
+        $deckCards =  json_encode(get_deck_cards($q));
 	$test = json_decode($deckCards);
 	$row = count($test);
 	for ($count =0; $count < $row; $count++){
