f4affb3ee02068cea4668870e1306f92e3040d67
picoriley <acloudy@mail.smu.edu>
2013-11-08 19:01:59 -0600

    Text input works!

--- Website/Game/js/game.js
+++ Website/Game/js/game.js
@@ -288,7 +288,7 @@ function Game()
       placeHolder: 'Enter an Answer...',
       backgroundColor: '#222222',
       borderColor: '#FF9900',
-      
+      boxShadow: '1px 1px 0px rgba(0, 0, 0, 1)'
     });
     //Check to see if we can use the canvas
     if (this.bgCanvas.getContext)
