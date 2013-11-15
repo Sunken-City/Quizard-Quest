490aee695626fb3b56b7ba93eea234e25325a6a3
nathanDmoore <moorednathan@gmail.com>
2013-11-15 16:03:22 -0600

    Typo

--- Website/js/gameMenu.js
+++ Website/js/gameMenu.js
@@ -2,21 +2,21 @@
 
 $(document).ready(function() {
 
-var JSON;
+	var JSON;
 
-/*\
-|*|		:: >> Display the Names of the User's decks << ::
-\*/
+	/*\
+	|*|		:: >> Display the Names of the User's decks << ::
+	\*/
 
-$.post("../API_Server/DeckReview.php", function(data){
-	JSON = $.parseJSON(data);
-	for( var i = 0, len = JSON.length;  i < len; i++){
-		var Deck = document.createElement("option");
-		Deck.value = JSON[i].deckID;
-		Deck.innerHTML = JSON[i].name;
-		document.getElementById('deckSelect').appendChild(Deck);
-	}		
-});
+	$.post("../API_Server/DeckReview.php", function(data){
+		JSON = $.parseJSON(data);
+		for( var i = 0, len = JSON.length;  i < len; i++){
+			var Deck = document.createElement("option");
+			Deck.value = JSON[i].deckID;
+			Deck.innerHTML = JSON[i].name;
+			document.getElementById('deckSelect').appendChild(Deck);
+		}		
+	});
 
 	/*\
 	|*|		:: >>Click listener for begin quest button<< ::
@@ -39,4 +39,4 @@ $.post("../API_Server/DeckReview.php", function(data){
 
 	});
 
-}
+});
