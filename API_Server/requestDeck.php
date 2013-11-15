25466de78dc9d0a2d5ca5efd1409a415dac21c04
nathanDmoore <moorednathan@gmail.com>
2013-11-15 17:22:55 -0600

    This should get all the deck cards for the game

---
+++ API_Server/requestDeck.php
@@ -0,0 +1,11 @@
+<?php
+	include "API.php"
+	session_start();
+
+	$deckID = $_SESSION['deckSelected'];
+
+	$deckCards = get_deck_cards($deckID);
+
+	echo json_encode($deckCards);
+
+?>
---
+++ Website/Game/js/requests.js
@@ -0,0 +1,66 @@
+$(document).ready(function() {
+	/*\
+	|*|		:: >> Retrieve Deck Data << ::
+	|*|
+	|*|		# Get DeckID through session variable
+	|*|		# Get Card data
+	|*|			-> card difficulty
+	|*|			-> card answer
+	|*|			-> card question
+	|*|			-> card category
+	|*|		# Put deck cards into array
+	|*|
+	\*/
+
+	/*\
+	|*|		:: >> Deck Object << ::
+	\*/
+
+	function Deck() {
+		var cards = array();
+	}
+
+	/*\
+	|*|		:: >> Card Object << ::
+	\*/
+
+	function Card() {
+		var category;
+		var difficulty;
+		var question;
+		var answer;
+	}
+
+	/*\
+	|*|		:: >> Request Deck Data << ::
+	\*/
+
+	var deck = new Deck();
+	var data;
+
+	$.post("../../API_Server/requestDeck.php",sendData,function(returnData) {
+
+		data = returnData;
+
+	},"json");
+
+	/*\
+	|*|		:: >> Initialize the New Deck << ::
+	\*/
+
+	for (int i = 0, len = data.length; i++) {
+		var newCard = new Car();
+		newCard.question = data['question'];
+		alert('question' + newCard.question);
+		newCard.answer = data['answer'];
+		newCard.difficulty = data['difficulty'];
+		newCard.category = data['category'];
+
+		deck.cards.push(newCard);
+	}
+
+	function returnDeck () {
+		return deck;
+	}
+
+});	
