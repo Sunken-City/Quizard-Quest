95a40c0895fcc6bc5df2b5bb8015b7f155935d19
picoriley <acloudy@mail.smu.edu>
2013-11-16 15:55:44 -0600

    Trying to ignore case when answering questions

--- Website/Game/js/game.js
+++ Website/Game/js/game.js
@@ -183,7 +183,7 @@ function loseLife() {
 
 function submitAnswer() {
   if (gamePlaying) {
-    if (currCard.answer == game.input._value) {
+    if (currCard.answer.toLower() === game.input._value.toLower()) {
       nextCard();
     }
     else {
