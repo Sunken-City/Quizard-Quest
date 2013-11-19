91d569e4b786ebc5003beb0e06f21bed20b63ba9
nathanDmoore <moorednathan@gmail.com>
2013-11-19 16:02:19 -0600

    Working on how to use Facebook login

--- Website/js/index.js
+++ Website/js/index.js
@@ -23,7 +23,6 @@ $(document).ready(function() {
 			  // The response object is returned with a status field that lets the app know the current
 			  // login status of the person. In this case, we're handling the situation where they 
 			  // have logged in to the app.
-
 			} else if (response.status === 'not_authorized') {
 			  // In this case, the person is logged into Facebook, but not into the app, so we call
 			  // FB.login() to prompt them to do so. 
@@ -52,7 +51,7 @@ $(document).ready(function() {
 			    // and signed request each expire
 			    var uid = response.authResponse.userID;
 			    var accessToken = response.authResponse.accessToken;
-			    window.location.href = "mainMenu.php";
+			    //logIn(uid,accessToken);
 			  } else if (response.status === 'not_authorized') {
 			    // the user is logged in to Facebook, 
 			    // but has not authenticated your app
@@ -103,29 +102,19 @@ $(document).ready(function() {
 		    });
 	  }
 
+	 //  function logIn(uid, accessToken) {
+
+	 //  	var formData = {logInFrom:"Facebook", id:uid};
+
+		// $.post("../API_Server/logIn.php",formData,function(){			
+		// });
+
+	 //  }
+
 	var newHeight = $("#logInSection").height()*1.6;
 	$("#headerIndex").css({
 		'height':newHeight + 'px',
 	});
-
-	// FB.getLoginStatus(function(response) {
-	// 	  if (response.status === 'connected') {
-	// 	    // the user is logged in and has authenticated your
-	// 	    // app, and response.authResponse supplies
-	// 	    // the user's ID, a valid access token, a signed
-	// 	    // request, and the time the access token 
-	// 	    // and signed request each expire
-	// 	    var uid = response.authResponse.userID;
-	// 	    var accessToken = response.authResponse.accessToken;
-	// 	    window.location.href = "mainMenu.php";
-	// 	  } else if (response.status === 'not_authorized') {
-	// 	    // the user is logged in to Facebook, 
-	// 	    // but has not authenticated your app
-	// 	    FB.login();
-	// 	  } else {
-	// 	    // the user isn't logged in to Facebook.
-	// 	  }
-	//  });
 		
 	/*\
 	|*|				:: >> Authenticate User After Login << ::
