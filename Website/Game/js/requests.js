702d48455f066deb070d03d6772fcad6f1bdb5a3
nathanDmoore <moorednathan@gmail.com>
2013-11-21 16:06:57 -0600

    Debug: gameMode variable is not reading mode passed to it

--- Website/Game/js/requests.js
+++ Website/Game/js/requests.js
@@ -14,6 +14,7 @@
 var deck1 = new Deck();
 var cardArray = [];
 var data;
+var mode;
 
 //+ Jonas Raoni Soares Silva
 //@ http://jsfromhell.com/array/shuffle [v1.0]
@@ -89,9 +90,8 @@ function initDeck() {
 /*\
 |*|   :: >> Fetch the Game Mode << ::
 \*/
-  
+
 sendData = {mode:'true'};
-var mode;
 
 $.post("../../API_Server/requestDeck.php",sendData,function(returnData) {
   data = $.parseJSON(returnData);
