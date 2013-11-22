48ffff34484ecb813c50d4121d8ecf3524b63b6a
moosemn456 <jgstangelo@gmail.com>
2013-11-22 15:00:23 -0600

    Mayhaps added XP and Gold

--- Website/Game/js/game.js
+++ Website/Game/js/game.js
@@ -23,6 +23,7 @@ var lives;
 var question;
 var answer;
 
+var goldEarned;
 var XPGained;
 var XPCategory = new array();
 
@@ -164,6 +165,7 @@ function loseLife() {
 function submitAnswer() {
   if (gamePlaying) {
     if (currCard.answer == game.input._value) {
+      numRight++;
       nextCard();
       XPGained += 10;
       switch(category){
@@ -184,7 +186,6 @@ function submitAnswer() {
       }
     }
     else {
-      numRight--;
       if (gameMode > GameMode.Training)
          loseLife();
       nextCard();
@@ -640,8 +641,37 @@ function animate() {
   
   else{
     document.getElementById('question').innerHTML = "WINNER WINNER CHICKEN DINNER! " + numRight + "/" + numCards;
-    var paragraph = 
-    document.creatElement('p'); 
+    
+    var results = document.creatElement('results');
+    results.innerHTML = "You gained " + XPGained + " total XP!";
+    
+    if (XPCategory[0] != 0)
+      results.innerHTML += "\nYou gained " + XPCategory[0] + " math XP!";
+    if (XPCategory[1] != 0)
+      results.innerHTML += "\nYou gained " + XPCategory[1] + " science XP!";
+    if (XPCategory[2] != 0)
+      results.innerHTML += "\nYou gained " + XPCategory[2] + " social studies XP!";
+    if (XPCategory[3] != 0)
+      results.innerHTML += "\nYou gained " + XPCategory[3] + " english XP!";
+    if (XPCategory[4] != 0)
+      results.innerHTML += "\nYou gained " + XPCategory[4] + " language XP!";
+    if (XPCategory[5] != 0)
+      results.innerHTML += "\nYou gained " + XPCategory[5] + " misc XP!";
+    
+    switch(gameMode) {
+      case 1:
+         results.innerHTML += "\nYou gained 100 gold!";
+         goldEarned = 100;
+      case 2:
+         results.innerHTML += "\nYou gained 300 gold!";
+         goldEarned = 300;
+      case 3:
+         results.innerHTML += "\nYou gained 500 gold!";
+         goldEarned = 500;
+      default:
+         console.log("No gamemode found! Oh schiesse!");
+    }
+    
   }
    
   if (gameMode > GameMode.Training) {
@@ -682,11 +712,12 @@ function init() {
     gameMode = md;
     deck.shuffle();
     numCards = deck.cards.length;
-    numRight = numCards;
+    numRight = 0;
     
     for (var i=0; i<6; i++)
       XPCategory[i] = 0;
     
+    goldEarned = 0;
     avatarInc = 715 / numCards;
     currCard = deck.draw();
     lives = numCards - Math.ceil(numCards * .7) + 1;
