7eb0e0ea704a0e179d0d2befc3a202c9ca73301b
moosemn456 <jgstangelo@gmail.com>
2013-11-22 15:15:28 -0600

    banana

--- Website/Game/js/game.js
+++ Website/Game/js/game.js
@@ -652,7 +652,7 @@ function animate() {
   else{
     document.getElementById('question').innerHTML = "WINNER WINNER CHICKEN DINNER! " + numRight + "/" + numCards;
     
-    var results = document.createElement('div');
+    var results = document.createElement("div");
     results.innerHTML = "You gained " + XPGained + " total XP!";
     
     if (XPCategory[0] != 0)
