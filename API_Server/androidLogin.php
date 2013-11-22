a11a3f2280652260a1782b4aa2402e299d44f47f
moosemn456 <jgstangelo@gmail.com>
2013-11-22 14:44:50 -0600

    Added a parens

--- API_Server/androidLogin.php
+++ API_Server/androidLogin.php
@@ -5,10 +5,11 @@
    $username = $_POST['username'];
    $password = $_POST['password'];
    
-   if(validate_password($username, $password) {
+   if(validate_password($username, $password)) {
       $_SESSION['userID'] = get_userID($username);
       return json_encode(get_deck_names());
    }
    else
       return 239;
 ?>
+
