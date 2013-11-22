1ccd0ca337ab83a3a9a953e0dd1fd2c0aa9a6122
moosemn456 <jgstangelo@gmail.com>
2013-11-22 17:01:38 -0600

    Android fixes

--- API_Server/androidLogin.php
+++ API_Server/androidLogin.php
@@ -6,7 +6,7 @@
    $password = $_POST['password'];
    
    if(validate_password($username, $password)) {
-      $_SESSION['userID'] = get_userID($username);
+      $userID = get_userID($username);
       echo json_encode(get_deck_names($userID));
    }
    else
