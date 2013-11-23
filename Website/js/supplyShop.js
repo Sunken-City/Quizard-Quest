bca226d01331ed8a1f59403b0dc61ec77d328762
nathanDmoore <moorednathan@gmail.com>
2013-11-22 18:00:13 -0600

    I hope this displays gold in the shop

--- Website/js/supplyShop.js
+++ Website/js/supplyShop.js
@@ -9,14 +9,14 @@ $(document).ready(function () {
 function display() {
 
 	var gold;
+	var goldDisplay;
 
 	var sendData = {'life':'and death'};
 
 	$.post("../API_Server/supplyShopSetup.php",sendData,function(data) {
 		gold = data['gold'];
+		goldDisplay = "You have " + gold + " gold";
 	},"json");
-
-	var goldDisplay = "You have " + gold + " gold";
 
 	$("#displayGold").html(goldDisplay);
 
