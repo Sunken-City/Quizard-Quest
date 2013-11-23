9b66fce072af1247f6bdd6589f2b1c49c6aca847
Gustavo Castillo <gcastillo@smu.edu>
2013-11-23 17:30:38 -0600

    attempting to fix click listeners in mainmenu

--- Website/passwordReset.php
+++ Website/passwordReset.php
@@ -1,6 +1,6 @@
 <!DOCTYPE HTML>
 <!-- 
-	Check the Security Question 
+	Password Reset Page
 	Author : Gustavo Castillo
  -->
 <html lang="en">
@@ -12,19 +12,19 @@
 		<title>Password Reset</title>
 		<script type = "text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 		<script type = "text/javascript" src="js/navigation.js"></script>
+		<script type = "text/javascript" src="js/passwordReset.js"></script>
 		<link href='http://fonts.googleapis.com/css?family=Share+Tech+Mono|VT323' rel='stylesheet' type='text/css'>
 
 
 	</head>
 	
 	<body>
-			<form id="pwReset"  action="../API_Server/checkSecurityAnswer.php" method="POST" >
+			<form id="pwReset" action="../API_Server/checkSecurityAnswer.php" method="POST" >
 				<input type="text" placeholder="Username" id="user" name="user" required />
 				<button type="button" value="Get Question" id="getSecurity"> Get Question</button>
 				<div id="securityQuestion">
 				</div>
 			</form>
-
 
 
 			<footer>
