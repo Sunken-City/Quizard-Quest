9646f2ceee113ec80a3f2dfb9c5296697d113131
moosemn456 <jgstangelo@gmail.com>
2013-11-22 18:47:45 -0600

    Thinking bout programmin stuff

--- API_Server/androidGetDeckCards.php
+++ API_Server/androidGetDeckCards.php
@@ -2,7 +2,7 @@
    include "androidAPI.php";
    session_start();
    $SdeckID = $_POST['deckID'];
-   $deckID = int inval(mixed $SdeckID, $base = 10);
+   $deckID = int intval(mixed $SdeckID, $base = 10);
    echo json_encode(getDeckCards($deckID));
    
 ?>
