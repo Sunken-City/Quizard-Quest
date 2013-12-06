d71e2c5b3cf1d48e8d8044faee22932ef056c184
nathanDmoore <moorednathan@gmail.com>
2013-12-06 14:07:53 -0600

    I hope this works for facebook log in

--- Website/js/newUser.js
+++ Website/js/newUser.js
@@ -177,30 +177,26 @@ $(document).ready(function() {
 			'SecurityQuestion':SecurityQuestion
 		};
 
-		if (newpwd !== passCheck) {
+		
 
-			alert("You passwords do no match!");
+		$.post("../API_Server/createAccount.php",formData, function(data) {
 
-		} else {
+			if (data['success'] === 'username') {
+				// do failure things
+            	alert("That username is already in use!");
 
-			$.post("../API_Server/createAccount.php",formData, function(data) {
+        	} else if (data['success'] === 'email') {
+            	// do failure things
+            	alert("That email is already in use!");
 
-				if (data['success'] === 'username') {
-					// do failure things
-	            	alert("That username is already in use!");
+        	} else {
+        		// do successful things
+            	window.location.href = "mainMenu.php";
+        	}
 
-	        	} else if (data['success'] === 'email') {
-	            	// do failure things
-	            	alert("That email is already in use!");
+		},"json");
 
-	        	} else {
-	        		// do successful things
-	            	window.location.href = "mainMenu.php";
-	        	}
-
-			},"json");
-
-		}
+		
 
 		e.preventDefault();
 		
